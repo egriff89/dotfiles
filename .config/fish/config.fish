7bb07d0abfdfc28eb9e48bfff1a43cccc8190fd9
Eric Griffith <eric.griffith42@gmail.com>
2022-05-06 01:39:08 -0500

    Adding fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,76 @@
+# ~/.config/fish/config.fish
+# Author: Eric Griffith
+# http://www.gitlab.com/egriff89
+
+# Some portions borrowed from the following repos:
+#   https://gitlab.com/dwt1/dotfiles
+
+ ### ADDING TO THE PATH
+# First line removes the path; second line sets it.  Without the first line,
+# your path gets massive and fish becomes very slow.
+set -e fish_user_paths
+set -U fish_user_paths $HOME/.local/bin $HOME/.local/util $fish_user_paths
+
+if test -f $HOME/.emacs.d/bin
+   set -U fish_user_paths $HOME/.emacs.d/bin $fish_user_paths
+end
+
+### EXPORT ###
+set fish_greeting                                 # Supresses fish's intro message
+set TERM "xterm-256color"                         # Sets the terminal type
+set EDITOR "emacsclient -t -a ''"                 # $EDITOR use Emacs in terminal
+set VISUAL "emacsclient -c -a emacs"              # $VISUAL use Emacs in GUI mode
+
+### "nvim" as manpager
+set -x MANPAGER "nvim -c 'set ft=man' -"
+
+### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
+set fish_color_normal brcyan
+set fish_color_autosuggestion '#7d7d7d'
+set fish_color_command brcyan
+set fish_color_error '#ff6c6b'
+set fish_color_param brcyan
+
+
+### ALIASES ###
+
+# navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+
+# listing files
+alias ls='ls -a --color=auto'
+alias ll='ls -lh'
+alias la='ls -alh'
+
+# vim and emacs
+alias vim='nvim'
+alias em='/usr/bin/emacs -nw'
+alias emacs="emacsclient -c -a 'emacs'"
+
+# pacman
+alias pacsyu='sudo pacman -Syu'                  # update only standard pkgs
+alias pacsyyu='sudo pacman -Syyu'                # Refresh pkglist & update standard pkgs
+alias unlock='sudo rm /var/lib/pacman/db.lck'    # remove pacman lock
+alias cleanup='sudo pacman -Rns (pacman -Qtdq)'  # remove orphaned packages
+
+# get fastest mirrors
+alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
+alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist"
+alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
+alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist"
+
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# Dotfiles bare repo alias
+alias dtf='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+
+
+### INIT STARSHIP PROMPT
+starship init fish | source
