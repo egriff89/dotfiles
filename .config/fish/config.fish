c4014ecacb3005265d01c3a768257a427a33667b
Eric Griffith <eric.griffith42@gmail.com>
2023-01-07 01:33:23 -0600

    Remove '--noconfirm' from paru aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -59,8 +59,8 @@ alias pacsyu='sudo pacman -Syu'                 # update only standard pkgs
 alias pacsyyu='sudo pacman -Syyu'               # Refresh pkglist & update standard pkgs
 alias pacss='pacman -Ss'                        # Search for standard pkgs
 alias parssa='paru -Ssa'                        # Search for AUR pkgs
-alias parsua='paru -Sua --noconfirm'            # update only AUR pkgs
-alias parsyu='paru -Syu --noconfirm'            # update standard pkgs and AUR pkgs
+alias parsua='paru -Sua'                        # update only AUR pkgs
+alias parsyu='paru -Syu'                        # update standard pkgs and AUR pkgs
 alias unlock='sudo rm /var/lib/pacman/db.lck'   # remove pacman lock
 alias cleanup='sudo pacman -Rns (pacman -Qtdq)' # remove orphaned packages
 
