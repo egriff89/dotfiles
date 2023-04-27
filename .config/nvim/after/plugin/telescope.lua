80549f54526f9d654510b268cb1a072069f08206
Eric Griffith <eric.griffith42@gmail.com>
2023-04-27 00:29:24 -0500

    Renamed telecope.lua to telescope.lua, fixed minor typos

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,5 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Search [p]roject [p]iles" })
+vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Search [p]roject [f]iles" })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Search files in Git repo" })
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
