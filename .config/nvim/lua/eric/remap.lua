e7eb8554705f9ed639d1add6a024d010e6db1c77
Eric Griffith <eric.griffith42@gmail.com>
2023-05-12 12:55:07 -0500

    Remove quickfix remaps

--- .config/nvim/lua/eric/remap.lua
+++ .config/nvim/lua/eric/remap.lua
@@ -31,12 +31,6 @@ vim.keymap.set("n", "Q", "<nop>")
 -- Format text
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format text" })
 
--- Quickfix navigation
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
 -- Replace word currently under cursor
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
     { desc = "Replace word under cursor" })
