bd54dd44a1782d85394b42471ff974997f118a94
Eric Griffith <eric.griffith42@gmail.com>
2023-08-28 01:27:40 -0500

    Add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,18 @@
+-- ~/.config/wezterm/wezterm.lua
+-- Author: Eric Griffith
+-- http://www.gitlab.com/egriff89
+
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+config.color_scheme = 'DoomOne'
+config.window_background_opacity = 0.90
+config.font = wezterm.font 'FiraCode Nerd Font'
+config.font_size = 11.0
+
+return config
