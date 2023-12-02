7e897f366c1638903e9defd6cbebf3582e714893
Eric Griffith <eric.griffith42@gmail.com>
2023-12-01 23:53:28 -0600

    Upgrade to Harpoon 2

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -1,10 +1,11 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
+local harpoon = require("harpoon")
 
-vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Mark file (Harpoon)" })
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+harpoon:setup()
 
-vim.keymap.set("n", "<C-u>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-i>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-o>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-p>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end, { desc = "Append to Harpoon list" })
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<C-u>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-i>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-o>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-p>", function() harpoon:list():select(4) end)
