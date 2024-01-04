8c79aba53fb99cbc0eb6ff96a39e5b9cfb425cc0
Eric Griffith <eric.griffith42@gmail.com>
2024-01-04 12:19:58 -0600

    Remove lazy option from Harpoon

--- .config/nvim/lua/eric/lazy/harpoon.lua
+++ .config/nvim/lua/eric/lazy/harpoon.lua
@@ -2,7 +2,6 @@ return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
   dependencies = "nvim-lua/plenary.nvim",
-  lazy = true,
  
   config = function() 
       local harpoon = require("harpoon")
