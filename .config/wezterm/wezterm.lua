9baac9c96ead6ddbe6e24a29208d9b167aa0f1e4
Eric Griffith <eric.griffith42@gmail.com>
2024-01-09 22:19:47 -0600

    Add keybinds for wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,6 +3,7 @@
 -- http://www.gitlab.com/egriff89
 
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 local config = {}
 
@@ -14,5 +15,18 @@ config.color_scheme = 'DoomOne'
 config.window_background_opacity = 0.90
 config.font = wezterm.font 'FiraCode Nerd Font'
 config.font_size = 09.0
+
+config.keys = {
+    {
+        key = "t",
+        mods = "SHIFT|ALT",
+        action = act.SpawnTab "CurrentPaneDomain",
+    },
+    {
+        key = "w",
+        mods = "SHIFT|ALT",
+        action = wezterm.action.CloseCurrentTab { confirm = true },
+    }
+}
 
 return config
