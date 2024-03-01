c62c0dbaa6621bcc09226b96e8c5464818fe7b3d
Eric Griffith <eric.griffith42@gmail.com>
2024-03-01 01:36:00 -0600

    add telescope-fzf-native.nvim

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,10 @@
 return {
 	"nvim-telescope/telescope-ui-select.nvim",
 	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+	},
+	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.5",
 		dependencies = { "nvim-lua/plenary.nvim" },
@@ -11,6 +15,12 @@ return {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown({}),
 					},
+					fzf = {
+						fuzzy = true,
+						override_generic_sorter = true,
+						override_file_sorter = true,
+						case_mode = "smart_case", -- also, "ignore_case" or "respect_case"
+					},
 				},
 			})
 			local builtin = require("telescope.builtin")
@@ -18,6 +28,7 @@ return {
 			vim.keymap.set("n", "<leader>gf", builtin.live_grep, { desc = "Grep in files" })
 
 			require("telescope").load_extension("ui-select")
+      require("telescope").load_extension("fzf")
 		end,
 	},
 }
