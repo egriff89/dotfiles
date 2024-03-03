7027f11b550580a30d860b68b78ccf7f47cbf0c9
Eric Griffith <eric.griffith42@gmail.com>
2024-03-02 22:47:09 -0600

    small updates to wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,25 +8,31 @@ local act = wezterm.action
 local config = {}
 
 if wezterm.config_builder then
-    config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
-config.color_scheme = 'DoomOne'
+-- Appearance settings
+config.color_scheme = "DoomOne"
 config.window_background_opacity = 0.90
-config.font = wezterm.font 'FiraCode Nerd Font'
+config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 09.0
 
+-- Tab bar settings
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false -- Retro appearance
+
+-- Keybinds
 config.keys = {
-    {
-        key = "t",
-        mods = "SHIFT|ALT",
-        action = act.SpawnTab "CurrentPaneDomain",
-    },
-    {
-        key = "w",
-        mods = "SHIFT|ALT",
-        action = wezterm.action.CloseCurrentTab { confirm = true },
-    }
+  {
+    key = "t",
+    mods = "SHIFT|ALT",
+    action = act.SpawnTab("CurrentPaneDomain"),
+  },
+  {
+    key = "c",
+    mods = "SHIFT|ALT",
+    action = act.CloseCurrentTab({ confirm = true }),
+  },
 }
 
 return config
