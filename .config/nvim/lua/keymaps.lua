1c08a12c3ab809b8e6085ae8a0734c14146d5ee5
Eric Griffith <eric.griffith42@gmail.com>
2024-03-19 01:11:27 -0500

    fix Oil keymap

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -28,7 +28,7 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- Open Oil for file navigation
-vim.keymap.set('n', '<leader>o', '<cmd>:Oil --float .<CR>', { desc = 'Oil' })
+vim.keymap.set('n', '<leader>o', '<cmd>:Oil --float<CR>', { desc = 'Oil' })
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
