45eb59038f667f6644832ee9fab15f3112f6a9a8
Eric Griffith <eric.griffith42@gmail.com>
2024-10-27 10:56:25 -0500

    add nvim-ansible plugin

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -11,6 +11,9 @@ return {
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
+  -- Ansible support
+  'mfussenegger/nvim-ansible',
+
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
