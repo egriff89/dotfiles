3a0a2881196d3777e21e84cf5c0eebccc1193da2
Eric Griffith <eric.griffith42@gmail.com>
2025-06-22 17:59:09 -0500

    chore(nvim): update servers

--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -98,15 +98,13 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        ansiblels = {},
         bashls = {},
         gopls = {},
         jsonls = {},
-        basedpyright = {},
         pylsp = {},
         ruff = {},
         taplo = {},
+        ty = {},
 
         lua_ls = {
           -- cmd = {...},
