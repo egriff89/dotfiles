a3a156c074da9479be216fe10e068f0e827b71ad
Eric Griffith <eric.griffith42@gmail.com>
2025-07-04 03:21:05 -0500

    chore(fish): slim down alias list

--- .config/fish/alias.fish
+++ .config/fish/alias.fish
@@ -1,12 +1,5 @@
 #!/usr/bin/env fish
 
-# navigation
-alias ..='cd ..'
-alias ...='cd ../..'
-alias .3='cd ../../..'
-alias .4='cd ../../../..'
-alias .5='cd ../../../../..'
-
 # listing files
 alias ls='eza --icons --color=always --group-directories-first'
 alias ll='eza -lg --icons --color=always --group-directories-first --header'
@@ -16,32 +9,15 @@ alias la='eza -lag --icons --color=always --group-directories-first --header'
 alias nv='/usr/bin/nvim'
 alias zd='/usr/bin/zeditor'
 
-# pacman / paru
-alias pacs='sudo pacman -S' # Install standard pkgs
-alias pacsyu='sudo pacman -Syu' # update only standard pkgs
-alias pacsyyu='sudo pacman -Syyu' # Refresh pkglist & update standard pkgs
-alias pacss='pacman -Ss' # Search for standard pkgs
-alias pacqi='pacman -Qi' # Show info for installed pkg
-alias pacqs='pacman -Qs' # Query for local pkgs
-alias pacq='pacman -Q' # Show installed pkgs w/ version number
-alias pacqm='pacman -Qm' # Show installed foreign pkgs w/ version number (AUR)
-alias parsa='paru -Sa' # Install AUR pkgs
-alias parssa='paru -Ssa' # Search for AUR pkgs
-alias parsua='paru -Sua' # update only AUR pkgs
-alias parsyu='paru -Syu' # update standard pkgs and AUR pkgs
-alias unlock='sudo rm /var/lib/pacman/db.lck' # remove pacman lock
+# pacman
+alias unlock='sudo rm /var/lib/pacman/db.lck'   # remove pacman lock
 alias cleanup='sudo pacman -Rns (pacman -Qtdq)' # remove orphaned packages
+alias orphan='pacman -Qtdq'                     # list orphaned packages
 
 # PNPM
 alias pn='pnpm'
 alias pnsu='pn self-update' # Self-update PNPM
-alias pnupg='pn update -g' # Update all global Node.js modules
-
-# flatpak
-alias fp='flatpak'
-alias fpup='flatpak update'
-alias fpin='flatpak install'
-alias fpun='flatpak uninstall'
+alias pnupg='pn update -g'  # Update all global Node.js modules
 
 ### other system utilities
 alias grep='grep --color=auto'
@@ -50,12 +26,6 @@ if test (pacman -Qq | grep 'bat-extras')
     alias diff='batdiff'
     alias man='batman'
 end
-
-# tar compression
-alias tarxz='tar -cvJf' # Create archive with xz compression (*.tar.xz)
-alias tarbz='tar -cvjf' # Create archive with bzip2 compression (*.tar.bz2)
-alias targz='tar -cvzf' # Create archive with gzip compression (*.tar.gz)
-alias tarcmp='tar -cvZf' # Create archive using compress (*.tar.Z)
 
 # get fastest mirrors
 alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
@@ -69,11 +39,3 @@ alias dtfs='dtf status'
 alias dtfa='dtf add'
 alias dtfp='dtf push'
 alias dtfc='dtf commit'
-
-# Git aliases
-alias gits='git status'
-alias gita='git add'
-alias gitc='git commit'
-alias gitps='git push'
-alias gitco='git checkout'
-alias gitsw='git switch'
