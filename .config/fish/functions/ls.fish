9bb72634a014aa458826cc6db020297fc37e0b0a
Eric Griffith <eric.griffith42@gmail.com>
2025-07-08 02:44:12 -0500

    chore(fish): remove extra functions

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,11 +1,3 @@
 function ls
     eza --icons --color=always --group-directories-first
 end
-
-function ll
-    eza -lg --icons --color=always --group-directories-first --header
-end
-
-function la
-    eza -lag --icons --color=always --group-directories-first --header
-end
