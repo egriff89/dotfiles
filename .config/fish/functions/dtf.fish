d8eea828c34b593832480d520fff97e09e5fe504
Eric Griffith <eric.griffith42@gmail.com>
2025-07-12 11:48:29 -0500

    chore(fish): add comments to `dtf`

--- .config/fish/functions/dtf.fish
+++ .config/fish/functions/dtf.fish
@@ -1,25 +1,47 @@
+## WIP
+## Need to find a way to refactor, but I suck at fish :(
+
 function dtf --wraps git -d "Manage dotfiles"
+    # default args for bare dotfiles repo
     set args "--git-dir=$HOME/.dotfiles --work-tree=$HOME"
+
+    # git commit
     if test "$argv[1]" = c
         eval git "$args" commit -m \"$argv[2]\"
+
+        # git status
     else if test "$argv[1]" = s
         eval git "$args" status
+
+        # git add
     else if test "$argv[1]" = a
         eval git "$args" add "$argv[2]"
+
+        # git push
     else if test "$argv[1]" = ps
+
+        # github mirror
         if test "$argv[2]" = gh
             eval git "$args" push "$argv[2]" master
         else
             eval git "$args" push
         end
+
+        # git pull
     else if test "$argv[1]" = pl
         eval git "$args" pull
+
+        # git diff
     else if test "$argv[1]" = d
+
+        # staged changes
         if test "$argv[2]" = s
             eval git "$args" diff --staged "$argv[3]"
         else
             eval git "$args" diff "$argv[2]"
         end
+
+        # default git
     else
         eval git "$args" "$argv[1]"
     end
