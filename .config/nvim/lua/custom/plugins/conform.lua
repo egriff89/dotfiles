72246fa1976d5c1f8206b1aaa915b402a3718322
Eric Griffith <eric.griffith42@gmail.com>
2025-07-14 05:14:10 -0500

    chore(nvim): break conform.nvim out into its own file

---
+++ .config/nvim/lua/custom/plugins/conform.lua
@@ -0,0 +1,31 @@
+return { -- Autoformat
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format { async = true }
+      end,
+      mode = 'n',
+      desc = '[F]ormat Buffer',
+    },
+  },
+
+  ---@module "conform"
+  ---@type conform.setupOpts
+  opts = {
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      python = { 'ruff' },
+      go = { 'gofumpt', 'goimports' },
+    },
+    default_format_opts = { lsp_format = 'fallback' },
+    format_on_save = { timeout_ms = 500 },
+    notify_on_error = false,
+  },
+  init = function()
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
+}
