e83dd8a11a3a51d6a82d6f0262d03f838952ab63
Eric Griffith <eric.griffith42@gmail.com>
2025-07-14 05:40:55 -0500

    chore(nvim): update options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -18,6 +18,8 @@ vim.opt.mouse = 'a'
 -- Don't show the mode, since it's already in status line
 vim.opt.showmode = false
 
+vim.opt.termguicolors = true
+
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
