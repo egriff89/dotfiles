f42d02c816cfaa4ec24def354fc36e0f1b7c7bac
Eric Griffith <griffith.eric@pm.me>
2025-09-18 22:11:21 -0500

    chore: add keymap for `:bdelete`

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -27,6 +27,8 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<leader>bd', '<cmd>:bdelete<CR>', { desc = '[B]uffer [D]elete' })
+
 -- Open Oil for file navigation
 vim.keymap.set('n', '<leader>o', '<cmd>:Oil --float<CR>', { desc = 'Oil' })
 
