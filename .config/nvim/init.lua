bdc8037cd200eee3d699c7a6dd4cf6b74d4558f1
Eric Griffith <griffith.eric@pm.me>
2025-09-18 22:10:12 -0500

    feat(nvim): add custom filetype for ghostty config for syntax highlighting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,6 @@
 require 'options'
 require 'keymaps'
+require 'filetypes'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
---
+++ .config/nvim/lua/filetypes.lua
@@ -0,0 +1,5 @@
+vim.filetype.add {
+  filename = {
+    ['~/.config/ghostty/config'] = 'ini',
+  },
+}
