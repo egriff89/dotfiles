be5d2834b7c2334a1f14109ffee307bf93be50f0
Eric Griffith <griffith.eric@pm.me>
2026-07-02 17:45:00 -0500

    chore(doom): enable literate config

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -164,7 +164,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        (ocaml +lsp)        ; an objective camel
-       org                 ; organize your plain life in plain text
+       (org +pretty)       ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
@@ -203,5 +203,5 @@
        ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
-       ;;literate
+       literate
        (default +bindings +smartparens))
